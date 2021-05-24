69d6334e0faaf84c4645b08bdaa388e61e94121f
Cristian Guilherme Petry <74006147+CrisPetry@users.noreply.github.com>
2021-05-23 23:56:53 -0300

    index adicionado e correções do login

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Página Principal</title>
+  <link href="Include/bootstrap.min.css" rel="stylesheet" />
+  <link href="Include/estilo.css" rel="stylesheet" />
+
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    body {
+      background-color: lightcyan;
+    }
+
+
+    body>main>div.row.mb-2>div:nth-child(2)>div>div.col.p-4.d-flex.flex-column.position-static {
+      background-color: whitesmoke;
+    }
+
+    body>main>div.row.mb-2>div:nth-child(1)>div>div.col.p-4.d-flex.flex-column.position-static {
+      background-color: whitesmoke;
+    }
+
+    body>nav{
+      background-color: whitesmoke;
+    }
+  </style>
+</head>
+
+<body>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+
+      <div class="container">
+        <header class="blog-header py-3">
+          <div class="row flex-nowrap justify-content-between align-items-center">
+            <div class="col-4 pt-1">
+            </div>
+            <div class="col-4 d-flex justify-content-end align-items-center">
+              <img src="Include/logo.png" alt="roupex" height="90" width="90">
+              </a>
+              <a class="btn btn-sm btn-outline-secondary" href="View/login.html"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
+                  <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
+                </svg>Logar</a>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+  </nav>
+
+  <main class="container">
+    <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+      <div class="col-md-6 px-0">
+        <h1 class="display-4 fst-italic">
+          ROUPEX: Venha conhecer o novo!
+        </h1>
+        <p class="lead my-3">
+          Uma plataforma totalmente customizável e adaptável as suas necessidades.
+        </p>
+      </div>
+    </div>
+
+    <div class="row mb-2">
+      <div class="col-md-6">
+        <div class="
+              row
+              g-0
+              border
+              rounded
+              overflow-hidden
+              flex-md-row
+              mb-4
+              shadow-sm
+              h-md-250
+              position-relative
+            ">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-primary">Não deixe de conferir</strong>
+            <h3 class="mb-0">Controle do estoque</h3>
+            <div class="mb-1 text-muted">Março 15</div>
+            <p class="card-text mb-auto">
+              Agora ficará mais fácil ter o controle do seu estoque.
+            </p>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+              <rect width="100%" height="100%" id="vendas" fill="#55595c" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="
+              row
+              g-0
+              border
+              rounded
+              overflow-hidden
+              flex-md-row
+              mb-4
+              shadow-sm
+              h-md-250
+              position-relative
+            ">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-success">Novidades</strong>
+            <h3 class="mb-0">Módulo de Vendas</h3>
+            <div class="mb-1 text-muted">Abril 26</div>
+            <p class="mb-auto">
+              Agora ficou mais fácil administrar suas vendas.
+            </p>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+              <rect width="100%" height="100%" fill="#55595c" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row g-5">
+      <div class="col-md-8">
+        <h3 class="pb-4 mb-4 fst-italic border-bottom">Conheça nossa plataforma</h3><br>
+
+        <article class="blog-post">
+          <h2 class="blog-post-title">Controle de Estoque</h2>
+
+          <p>
+            Nosso software permite a gestão de seu estoque com recursos que facilitam a conferência,
+            a entrada e saída de produtos em estoque e muito mais.
+          </p>
+          <br>
+          <hr /><br>
+
+          <article class="blog-post">
+            <h2 class="blog-post-title">Controle de Vendas</h2>
+
+            <p>
+              Para ter total controle sobre as vendas em sua Loja de Roupas,
+              oferecemos uma ferramenta que possibilita a realização de vendas de forma prática e simples para você.
+            </p>
+            <br>
+            <hr /><br>
+
+            <article class="blog-post">
+              <h2 class="blog-post-title">Cadastro de Clientes</h2>
+
+              <p>
+                Mantenha atualizado o cadastro de clientes de sua Loja de Roupas. Cadastrando seu cliente,
+                contando com todas as suas informações,
+                se torna uma poderosa ferramenta de relacionamento, para aumentar sua fidelidade e faturamento.
+              </p>
+              <br>
+              <hr /><br>
+
+
+              <nav class="blog-pagination" aria-label="Pagination">
+                <a class="btn btn-outline-primary" href="#">Voltar ao Início</a>
+              </nav>
+      </div>
+
+      <div class="col-md-4">
+        <div class="position-sticky" style="top: 2rem">
+          <div class="p-4 mb-3 bg-light rounded">
+            <h4 class="fst-italic">Sobre</h4>
+            <p class="mb-0">
+              Crie um site moderno e exclusivo para lojas de roupas com uma ferramenta feita para construção sites e geração de negócios
+            </p>
+          </div>
+
+          <div class="p-4">
+            <h4 class="fst-italic">Ultimas Atualizações</h4>
+            <ol class="list-unstyled mb-0">
+              <li><a>Abril 2021</a></li>
+              <li><a>Maio 2021</a></li>
+            </ol>
+          </div>
+
+          <div class="p-4">
+            <h4 class="fst-italic">Redes</h4>
+            <ol class="list-unstyled">
+              <li><a href="https://github.com/CrisPetry/ProjDevWeb">GitHub</a></li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
